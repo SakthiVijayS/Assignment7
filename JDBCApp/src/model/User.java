937675555aed86714811ac43e474919d1073572d
SakthiVijayS <124193377+SakthiVijayS@users.noreply.github.com>
2023-11-15 21:29:00 -0500

    Add files via upload

---
+++ JDBCApp/src/model/User.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+import java.util.Date;
+import javax.swing.ImageIcon;
+import java.util.UUID;
+
+/**
+ *
+ * @author shakt
+ */
+public class User {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ImageIcon getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(ImageIcon profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    @Override
+    public String toString() {
+       return name ;
+    }
+    
+    
+    
+    
+    String name;
+    public int id;
+    int age;
+    public ImageIcon profilePic;
+    
+    
+    
+}
